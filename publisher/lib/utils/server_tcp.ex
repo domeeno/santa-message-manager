b1a0daba49dd5cd2e54fc1994ecbd6ec4721639b
dominatorfaf <dominicflocea.dev@gmail.com>
2021-06-28 04:50:44 +0300

    broker - publisher established connection

---
+++ publisher/lib/utils/server_tcp.ex
@@ -0,0 +1,16 @@
+defmodule Utils.ServerTCP do
+  def start_link(host, port) do
+    IO.puts(host)
+    IO.puts(port)
+    :gen_tcp.connect(host, port, [:binary, active: false])
+  end
+
+  def send(socket, data) do
+    size = data
+      |> String.length()
+      |> Integer.to_string()
+      |> String.pad_leading(5, "0")
+
+    :gen_tcp.send(socket, size <> data)
+  end
+end
--- publisher/lib/worker/elf_slave.ex
+++ publisher/lib/worker/elf_slave.ex
@@ -3,21 +3,20 @@ defmodule Worker.ElfWorker do
 
   def start_link(args) do
     index = Integer.to_string(args.active)
-    # IO.puts("elf_worker" <> index <> " starting...")
-    {:ok, pid} = GenServer.start_link(__MODULE__, :ok, name: :"elf_worker#{index}")
+    {:ok, socket} = Utils.ServerTCP.start_link('127.0.0.1', 4040)
+    {:ok, pid} = GenServer.start_link(__MODULE__, %{socket: socket}, name: :"elf_worker#{index}")
     Registry.register(Registry.ViaTest, "elf_worker" <> index, pid)
-    # IO.puts("elf_worker" <> index <> " started")
     {:ok, pid}
   end
 
-  def init(:ok) do
-    {:ok, %{}}
+  def init(state) do
+    {:ok, state}
   end
 
-  def handle_cast({:eval, message, index, worker_count}, _state) do
-    [_tweet, sentiment_q, _polarity] =  Utils.TweetProcess.eval(message)
+  def handle_cast({:eval, message, index, worker_count}, state) do
+    [tweet, sentiment_q, _polarity] =  Utils.TweetProcess.eval(message)
     IO.puts("worker: " <> Integer.to_string(index) <> ":\t sentiment: " <> Float.to_string(sentiment_q) <> ":\t workers count: " <> Integer.to_string(worker_count))
-
+    Utils.ServerTCP.send(state.socket, tweet)
     # Mongo.UploadServer.add_message(%{message: tweet, sentiment: sentiment_q, polarity: polarity})
     {:noreply, %{}}
   end
